3730fd2d096c635361e40b8a8f887a1dabe2ff42
ethanbitbucket <33522236+theethan@users.noreply.github.com>
2018-04-20 16:17:03 -0500

    "update nowstr to accept arguments"

--- nowstr.m
+++ nowstr.m
@@ -1,4 +1,4 @@
-function s = nowstr
+function s = nowstr(varargin)
 % NOWSTR Create a string for the current time
 %   S = NOWSTR constructs a string of the format yyyymmdd-HHMM~ where - and ~
 %   are special characters denoting the date and time respectively.  Default 
@@ -9,9 +9,13 @@ function s = nowstr
 
 % Might want to use some arguments for customisation?
 % --some other time.
+% --added now (hah.):
+d = 'd'; h = 'h'; % default
+if nargin>0 && ischar(varargin{1}), d = varargin{1}; end % input override
+if nargin>1 && ischar(varargin{2}), h = varargin{2}; end % input override
 
 n = now; % obsessively ensure that the date string 
 
-s = sprintf('%sd%sh',datestr(n,'yyyymmdd'),datestr(n,'HHMM'));
+s = sprintf('%s%s%s%s',datestr(n,'yyyymmdd'),d,datestr(n,'HHMM'),h);
 
 end
